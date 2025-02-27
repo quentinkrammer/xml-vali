85b8967a55760532fd9203a8ab904623349f7a03
quentinkrammer <bopatzable@gmail.com>
2025-02-27 19:34:01 +0100

    load xml

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { XMLParser } from "fast-xml-parser";
+import { useEffect } from "react";
+import "./App.css";
+import xmlData from "./assets/test.xml";
 
-function App() {
-  const [count, setCount] = useState(0)
+const parser = new XMLParser({
+  ignoreAttributes: false,
+});
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+async function createFile(url: string) {
+  const response = await fetch(url);
+  const data = await response.blob();
+  const metadata = {
+    type: "text/xml",
+  };
+  return new File([data], "test.xml", metadata);
 }
 
-export default App
+function App() {
+  useEffect(() => {
+    async function loadFile() {
+      const file = await createFile(xmlData);
+      const text = await file.text();
+      const parsed = parser.parse(text);
+
+      console.log(parsed);
+    }
+    loadFile();
+  });
+  return (
+    <>
+      <div>moin</div>
+    </>
+  );
+}
+
+export default App;
